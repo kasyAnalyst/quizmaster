1900f64c59954e48d359a5e8944a5ffef453a2d7
Prosper .N. Collins <kasyAnalyst@gmail.com>
2025-06-16 01:31:46 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 
-app, db, bcrypt = create_app()
+# Create the app instance - Vercel will find this
+app = create_app()
 
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
